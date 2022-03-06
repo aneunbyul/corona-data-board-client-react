23b97e1ea32841887cf7b15da0d746117d9b223b
star <kingbar222@naver.com>
2022-03-06 23:13:33 +0900

    [추가] Nav 기본 레이아웃 생성

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -1,23 +1,19 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import { useRouter } from "next/router";
-import { useEffect, useRef, useState } from "react";
+import Nav from "./Nav";
+import {useRouter} from "next/router";
+import {useEffect, useRef, useState} from "react";
 import styled from "styled-components";
-import { createContext } from "react";
+import {createContext} from "react";
 
-export const HeaderBgContext = createContext();
-
-const Layout = ({ children }) => {
-  const [isShowHeaderBg, setIsShowHeaderBg] = useState(false);
-  const [visible, setVisible] = useState(true);
-  const router = useRouter();
-  const isProductsPage = router.pathname.includes("products");
+const Layout = ({children}) => {
   return (
-    <HeaderBgContext.Provider value={{ isShowHeaderBg, setIsShowHeaderBg }}>
-        <Header visible={visible} setVisible={setVisible} />
-        <main>{children}</main>
-        {!isProductsPage && <Footer />}
-    </HeaderBgContext.Provider>
+    <>
+      <Header/>
+      <Nav/>
+      <main>{children}</main>
+      <Footer/>
+    </>
   );
 };
 
---
+++ src/components/layouts/Nav.js
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+const drawerWidth = 240;
+
+const Nav = () => {
+  return (
+    <>
+      <Box sx={{display: 'flex'}}>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            zIndex: -1,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <Toolbar/>
+          <Toolbar/>
+          <List>
+            {['국내종합 현황', '시도별 현황', '전세계 대시보드'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
+                </ListItemIcon>
+                <ListItemText primary={text}/>
+              </ListItem>
+            ))}
+          </List>
+          <Divider/>
+          <List>
+            {['해외 종합 현황', '주변 국가별 현황'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
+                </ListItemIcon>
+                <ListItemText primary={text}/>
+              </ListItem>
+            ))}
+          </List>
+          <Divider/>
+          <List>
+            {['거리두기정보', '백신접종센터정보', '주요 소식',].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
+                </ListItemIcon>
+                <ListItemText primary={text}/>
+              </ListItem>
+            ))}
+          </List>
+          <Divider/>
+          <List>
+            {['데이터 출처'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
+                </ListItemIcon>
+                <ListItemText primary={text}/>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+      </Box>
+    </>
+  );
+};
+
+export default Nav;

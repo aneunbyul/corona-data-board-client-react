271e023b9e0be8373c014bc0efbc33933e35c2fa
star <kingbar222@naver.com>
2022-03-17 00:38:19 +0900

    [추가] main table 생성

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Table from '../src/components/contents/Main/Table'
 
 import BaseHead from "../src/components/base/BaseHead";
 import styled from "styled-components";
@@ -9,8 +10,19 @@ const Main = () => {
   return (
     <>
       <BaseHead title="MAIN"/>
+      <StyledMain>
+        <Table/>
+      </StyledMain>
     </>
   );
 };
+
+const StyledMain = styled.main`
+  position: absolute;
+  left: 240px;
+  width: calc(100vw - 240px);
+  top: 80px;
+  height: calc(100vh - 80px);
+`;
 
 export default Main;
